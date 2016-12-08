95eea52313dffcbad0c73aa0859a6a769d429227
adusuv <adusuv@gmail.com>
2016-12-08 10:57:06 -0600

    First Commit

---
+++ InternetMemeCatalog/Controllers/ImageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InternetMemeCatalog.Models;
+
+namespace InternetMemeCatalog.Controllers
+{
+    public class ImageController : Controller
+    {
+        //
+        // GET: /Image/
+
+        public ActionResult Index()
+        {          
+
+            HallmarkEntities1 hallmarkEntities = new HallmarkEntities1();
+            List<Image> images = hallmarkEntities.Images.ToList();
+            return View(images);
+        }
+
+        public ActionResult Genre()
+        {
+            HallmarkEntities1 hallmarkEntities = new HallmarkEntities1();
+            var ImagesGrouped = from b in hallmarkEntities.Images
+                                group b by b.Genre into g
+                                select new Genre { Images = g, GenreName = g.Key };
+            return View(ImagesGrouped.ToList());
+        }
+
+
+        public ActionResult Delete(int Id, string ControllerName)
+        {
+                HallmarkEntities1 hallmarkEntities = new HallmarkEntities1();
+                Image img = hallmarkEntities.Images.Find(Id);
+                hallmarkEntities.Images.Remove(img);
+                hallmarkEntities.SaveChanges();           
+            return RedirectToAction(ControllerName);
+        }
+    }
+}
---
+++ InternetMemeCatalog/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InternetMemeCatalog.Models
+{
+
+    public class Genre
+    {
+        public string GenreName;
+        public IEnumerable<Image> Images;
+    }
+}
